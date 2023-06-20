563c6d5e8ad893074c70b2d5c163b72e614c0e6b
himanshuops <131866946+himanshuops@users.noreply.github.com>
2023-06-20 22:23:42 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-022e1a32d3f742bd8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "test-instance"
+  }
+}
