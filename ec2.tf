5a832b9b16af5c3ed31d113b69c6430f212d5e3c
himanshuops <131866946+himanshuops@users.noreply.github.com>
2023-06-21 11:41:24 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -14,9 +14,10 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami           = "ami-022e1a32d3f742bd8"
+
   instance_type = "t2.micro"
 
   tags = {
-    Name = "test-instance"
+    Name = "jaha"
   }
 }
